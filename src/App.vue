0c3b90e4b8f66f264e0c82f8b0c7fbe57d494aae
ziyin.lin <ziyin5030@gmail.com>
2025-05-14 14:09:27 +0800

    feat : login

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  max-width: 100vw;
+}
+#app {
+  margin: 0 !important;
+  padding: 0 !important;
+  max-width: 100vw;
+  overflow-x: hidden;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+import '@/assets/css/LoginView.css'
+
+const account = ref('')
+const password = ref('')
+const errorMsg = ref('')
+const router = useRouter()
+
+const handleLogin = async () => {
+  try {
+    const response = await axios.post('https://localhost:44357/api/authorization/login', {
+      account : account.value,
+      password: password.value
+    })
+
+    const result = response.data
+    
+    if (result.Success) {
+      const token = result.Message
+      localStorage.setItem('token', token)
+      errorMsg.value=''
+      router.push('/')
+    }
+    else {
+      errorMsg.value = result.Message || '登入失敗'
+    }
+  } catch (error) {
+    errorMsg.value = error?.response?.data?.Message || '登入時發生錯誤'
+  }
+}
+</script>
+
+<template>
+  <div class="page-wrapper">
+    <div class="login-container">
+      <div class="image-box"></div>
+      <form @submit.prevent="handleLogin">
+        <div class="form-group">
+          <label>帳號:</label>
+          <input type="text" v-model="account" />
+        </div>
+        <div class="form-group">
+          <label>密碼:</label>
+          <input type="password" v-model="password" />
+        </div>
+        <button type="submit" class="login-btn">登入</button>
+        <p v-if="errorMsg" class="error">{{ errorMsg }}</p>
+      </form>
+    </div>
+  </div>
+</template>
+
